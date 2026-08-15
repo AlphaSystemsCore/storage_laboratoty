9523ef2ba81c6da4f9fc2d9423c6c576a88a4cc1
earthbreaker <alphonceodongo005@gmail.com>
2026-08-15 23:21:58 +0300

    debugged validate_file

--- app/services/media_services.py
+++ app/services/media_services.py
@@ -33,31 +33,41 @@ async def validate_file(file: UploadFile):
     STORAGE_DIR.mkdir(exist_ok=True)
     old_filename = file.filename
     unique_filename = f"{uuid4()}{extension}"
-    new_filename = STORAGE_DIR / unique_filename
+    filename = STORAGE_DIR / unique_filename
     ALLOW_FILE_SIZE = 1024 * 1024 * 60
     CHUNK_SIZE = 1024 * 10
     total_byte_read = 0
 
     sha512 = hashlib.sha512()
     try:
-        with open(new_filename, "wb") as f:
+        with open(filename, "wb") as f:
             while chunk := await file.read(CHUNK_SIZE):
                 total_byte_read += len(chunk)
                 if total_byte_read > ALLOW_FILE_SIZE:
                     raise FileSizeTooLargeError(f"File size too large; Presented:{file.size / (1024 * 1024):.1f} MBs.; Allowed filesize: {ALLOW_FILE_SIZE/ (1024 *1024):.1f} MBs.")
-
                 sha512.update(chunk)
     except Exception as e:
-        if new_filename.exists():
-            new_filename.unlink()
-        print(f"{new_filename} cleaned up!")
+        if filename.exists():
+            filename.unlink()
+            print(f"{new_filename} cleaned up!")
+            print(old_filename)
+            print("Error: ", e)
         raise e
     finally:
         await file.close()
 
         return sha512.hexdigest()
 
-    
+    # metadata to be collected:
+    # new_filename
+    # old_filename
+    # Path
+    # checksum
+    # mime_type
+    # size
+    # created_at
+    # update_at
+
 
 
    
